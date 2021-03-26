db69e4367f08bc3c6ee88177d6f86ad6b416f1a1
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-03-26 01:30:02 +0100

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use crypto::{
 };
 
 
+// Create a cryptographically secure random mnemonic using Bip39.
 fn random_mnemonic() -> String {
     let mut random_data = [0u8; 32];
     fill(&mut random_data).unwrap();
@@ -21,6 +22,7 @@ fn random_mnemonic() -> String {
 }
 
 
+// Derive the private key from the mnemonic, the account index and the address index using Slip10.
 fn private_key_from_mnemonic(mnemonic: &str, account_index: u32, address_index: u32) -> SecretKey {
     let mut seed = [0u8; 64];
     mnemonic_to_seed(&mnemonic, &"", &mut seed);
@@ -33,17 +35,20 @@ fn private_key_from_mnemonic(mnemonic: &str, account_index: u32, address_index: 
 }
 
 
+// Derive the public key from the private key using Ed25519.
 fn public_key_from_private_key(private_key: SecretKey) -> PublicKey {
     private_key.public_key()
 }
 
 
+// Derive the address by hashing the public key using BLAKE2b256.
 fn ed25519_address_from_public_key(public_key: PublicKey) -> Ed25519Address {
     let hash = Blake2b256::digest(&public_key.to_compressed_bytes());
     Ed25519Address::new(hash.into())
 }
 
 
+// Derive the human readable address using Bech32.
 fn bech32_address_from_ed25519_address(ed25519_address: Ed25519Address, hrp: &str) -> Bech32Address {
     ed25519_address.to_bech32(hrp).into()
 }

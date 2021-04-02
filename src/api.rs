88029411ca69a3a572a7cbb04cac5d34ed84fb1b
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-04-02 20:26:34 +0200

    Use String types for all api functions

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,6 @@
-use iota::{Bech32Address, Ed25519Address};
+use std::{convert::TryInto, str::FromStr};
+
+use iota::Ed25519Address;
 use crypto::{
     hashes::{
         Digest,
@@ -8,7 +10,7 @@ use crypto::{
         bip39::{mnemonic_to_seed, wordlist},
         slip10::{Chain, Curve, Seed},
     },
-    signatures::ed25519::{PublicKey, SecretKey},
+    signatures::ed25519::SecretKey,
     utils::rand::fill,
 };
 
@@ -25,32 +27,41 @@ pub fn random_mnemonic() -> String {
 
 // Derive the private key from the mnemonic, the account index and the address index using SLIP-0010
 // See: https://github.com/satoshilabs/slips/blob/master/slip-0010.md
-pub fn private_key_from_mnemonic(mnemonic: &str, account_index: u32, address_index: u32) -> SecretKey {
+pub fn private_key_from_mnemonic(mnemonic: String, account_index: u32, address_index: u32) -> String {
     let mut seed = [0u8; 64];
-    mnemonic_to_seed(&mnemonic, &"", &mut seed);
+    mnemonic_to_seed(&mnemonic.as_str(), &"", &mut seed);
 
     let curve = Curve::Ed25519;
     let chain = Chain::from_u32_hardened(vec![44, 4218, account_index, false as u32, address_index]);
-    Seed::from_bytes(&seed)
+
+    let bytes = Seed::from_bytes(&seed)
         .derive(curve, &chain).unwrap()
         .secret_key().unwrap()
+        .to_le_bytes();
+    hex::encode(bytes)
 }
 
 
 // Derive the public key from the private key
-pub fn public_key_from_private_key(private_key: SecretKey) -> PublicKey {
-    private_key.public_key()
+pub fn public_key_from_private_key(private_key: String) -> String {
+    let bytes = hex::decode(private_key).unwrap().try_into().unwrap();
+    let key = SecretKey::from_le_bytes(bytes).unwrap();
+
+    let bytes = key.public_key().to_compressed_bytes();
+    hex::encode(bytes)
 }
 
 
 // Derive the address by hashing the public key using BLAKE2b256
-pub fn ed25519_address_from_public_key(public_key: PublicKey) -> Ed25519Address {
-    let hash = Blake2b256::digest(&public_key.to_compressed_bytes());
-    Ed25519Address::new(hash.into())
+pub fn ed25519_address_from_public_key(public_key: String) -> String {
+    let bytes = hex::decode(public_key).unwrap();
+    let hash = Blake2b256::digest(&bytes);
+    Ed25519Address::new(hash.into()).to_string()
 }
 
 
 // Derive the human readable address using Bech32
-pub fn bech32_address_from_ed25519_address(ed25519_address: Ed25519Address, hrp: &str) -> Bech32Address {
-    ed25519_address.to_bech32(hrp).into()
+pub fn bech32_address_from_ed25519_address(ed25519_address: String, hrp: String) -> String {
+    let address = Ed25519Address::from_str(&ed25519_address.as_str()).unwrap();
+    address.to_bech32(hrp.as_str())
 }
--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,18 @@ fn main() {
         );
     let matches = app.get_matches();
 
-    let mnemonic: String;
-
-    if let Some(mnemonic_argument) = matches.value_of("mnemonic") {
-        mnemonic = String::from(mnemonic_argument);
-    } else {
-        mnemonic = api::random_mnemonic();
-    }
+    let mnemonic = matches.value_of_t("mnemonic").unwrap_or(api::random_mnemonic());
     println!("Mnemonic: {}", mnemonic);
 
-    let private_key = api::private_key_from_mnemonic(mnemonic.as_str(), 0, 0);
-    println!("Private Key: {}", hex::encode(private_key.to_le_bytes()));
+    let private_key = api::private_key_from_mnemonic(mnemonic, 0, 0);
+    println!("Private Key: {}", private_key);
 
     let public_key = api::public_key_from_private_key(private_key);
-    println!("Public Key: {}", hex::encode(public_key.to_compressed_bytes()));
+    println!("Public Key: {}", public_key);
 
     let ed25519_address = api::ed25519_address_from_public_key(public_key);
     println!("Ed25519 Address: {}", ed25519_address);
 
-    let bech32_address = api::bech32_address_from_ed25519_address(ed25519_address, "atoi");
+    let bech32_address = api::bech32_address_from_ed25519_address(ed25519_address, String::from("atoi"));
     println!("Bech32 Address: {}", bech32_address);
 }
